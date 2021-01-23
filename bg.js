5412a4bfdd4f580f560d11bf8818d8a00c3a6ea3
Jin-hw <70885954+Jin-hw@users.noreply.github.com>
2021-01-23 19:12:13 +0900

    Make more beautiful by css

--- bg.js
+++ bg.js
@@ -1,20 +1,20 @@
 const body = document.querySelector("body");
 
-const IMG_NUMBER = 3;
+const IMG_NUMBER = 6;
 
-function paintImage(imgNumber){
+function paintImage(imgNumber) {
     const image = new Image();
     image.src = `images/${imgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.appendChild(image);
 }
 
-function genRandom(){
+function genRandom() {
     const number = Math.floor(Math.random() * IMG_NUMBER);
     return number;
 }
 
-function init(){
+function init() {
     const randomNumber = genRandom();
     paintImage(randomNumber);
 }
--- weather.js
+++ weather.js
@@ -6,14 +6,14 @@ const COORDS = 'coords';
 function getWeather(lat, lng) {
     fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
-        )
-        .then(function(response) {
+    )
+        .then(function (response) {
             return response.json();
         })
-        .then(function(json){
+        .then(function (json) {
             const temperature = json.main.temp;
             const place = json.name;
-            weather.innerText = `${temperature} @ ${place}`;
+            weather.innerText = `${temperature} °C @ ${place}`;
         });
 }
 
@@ -21,7 +21,7 @@ function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
-function handleGeoSucces(position){
+function handleGeoSucces(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     const coordsObj = {
@@ -36,13 +36,13 @@ function handleGeoError() {
     console.log("Can't access geo location");
 }
 
-function askForCoords(){
+function askForCoords() {
     navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
 }
 
-function loadCoords(){
+function loadCoords() {
     const loadedCoords = localStorage.getItem(COORDS);
-    if (loadedCoords === null){
+    if (loadedCoords === null) {
         askForCoords();
     } else {
         const parsedCoords = JSON.parse(loadedCoords);
@@ -50,7 +50,7 @@ function loadCoords(){
     }
 }
 
-function init(){
+function init() {
     loadCoords();
 }
 
